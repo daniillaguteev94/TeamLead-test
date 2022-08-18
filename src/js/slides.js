691b7aa1954747aa1e790aafbb72709263999acb
Daniil Laguteev <daniillaga121@yandex.ru>
2022-08-18 18:18:19 +0300

    сверстал 4 блока

---
+++ src/js/slides.js
@@ -0,0 +1,9 @@
+import Swiper from 'swiper';
+
+var swiper = new Swiper(".swipermain", {
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 10,
+  freeMode: true,
+  allowTouchMove: true
+});
