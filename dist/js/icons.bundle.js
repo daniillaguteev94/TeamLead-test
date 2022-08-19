b56902b5dfeec22cd7aa13b796ca16cb48899e87
Daniil Laguteev <daniillaga121@yandex.ru>
2022-08-19 10:23:48 +0300

    кнопка заказать

---
+++ dist/js/icons.bundle.js
@@ -0,0 +1,2 @@
+(()=>{var r,o={683:r=>{function o(r){var o=new Error("Cannot find module '"+r+"'");throw o.code="MODULE_NOT_FOUND",o}o.keys=()=>[],o.resolve=o,o.id=683,r.exports=o}},e={};function t(r){var n=e[r];if(void 0!==n)return n.exports;var s=e[r]={exports:{}};return o[r](s,s.exports,t),s.exports}t.o=(r,o)=>Object.prototype.hasOwnProperty.call(r,o),(r=t(683)).keys().forEach(r),fetch("../img/icons/icons.svg").then((function(r){return r.text()}))})();
+//# sourceMappingURL=icons.bundle.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,19 @@ import '../scss/main.scss';
 
 import '../js/slides';
 import '../js/timer';
+
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+}
